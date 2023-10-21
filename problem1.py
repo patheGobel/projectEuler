8ebdd1f168ef4a5d0396a1d1fc2526fbd5d849a3
Pathe <pathegba@gmail.com>
2023-10-21 15:34:34 +0000

    dans les deux précédant code, j'avais utilser des listes pour stocker séparément les multiples de 3 et de 5. Ce qui Entrainnait qu'on avait des doublons. Dans cette version, j'ai donné au Or tout son sens

--- problem1.py
+++ problem1.py
@@ -4,10 +4,10 @@ If we list all the natural numbers below 10 that are multiples of 3 or 5, we get
 
 Find the sum of all the multiples of 3 or below 1000
 """
-liste1 = [3 * x for x in range(1, 10) if 3 * x < 10]
-sum1 = sum(liste1)
+total_sum = 0
 
-liste2 = [5 * x for x in range(1, 10) if 5 * x < 10]
-sum2 = sum(liste2)
+for number in range(1, 1000):
+    if number % 3 == 0 or number % 5 == 0:
+        total_sum += number
 
-print(sum1+sum2)
+print(total_sum)
