5254b24cffb15c1738065611bf8003c3d9913754
Pathe <pathegba@gmail.com>
2023-10-25 18:14:07 +0000

    ça marche mais a optimiser grave

--- problem3.py
+++ problem3.py
@@ -5,21 +5,6 @@
     What is the largest prime factor of the number 600851475143 ?
 """
 
-"""
-def largestPrimeFactorOfTheNumber(a):
- 
-        if a/isPremier(a)==0:
-            return isPremier(a)
-
-def isPremier(a):
-    b=a
-    while b>3 :
-        b=b-1
-
-m=600851475143    
-n = int(m/2)
-print(f"the largest prime factor of the{m} is {largestPrimeFactorOfTheNumber(n)}") """
-
 def isPremier(n):
     if n <= 1:
         return False
@@ -36,20 +21,17 @@ def isPremier(n):
 
 def largestPrimeFactorOfTheNumber(a):
 
-    x=int(a/2)
-
-    while x>3:
+  x=int(a/2)
+  while (x>3):
+    if (x-1)%6==0 or (x+1)%6 == 0:
+      if ((x)%2!=0) and (x%5!=0):
         if isPremier(x):
+          if a%(x)==0:
+            return x
+            break
+          #print(f"{x} est un un nombre premier qui divise {a}")
+    x-=1
 
-            if a%x==0:
-                return x
-                break
-        x-=1
-        
-
-
-n=600851475143 
+#600851475143
+n=600851475143
 print(f"the largest prime factor of the {n} is {largestPrimeFactorOfTheNumber(n)}")
-
-
-    
