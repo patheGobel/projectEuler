0643e9cd5fad8e172e1257d503b79562fb3a8e0e
Pathe <pathegba@gmail.com>
2023-11-03 01:28:27 +0000

    j'ai changé de paradigme en utilisant une liste

--- tester.py
+++ tester.py
@@ -3,10 +3,19 @@ def largestPalindrome3Digit(n):
     n = str(n)
     if n[0:2] == n[:-2]:
         print("palindrome")
-        if isPremier(n):
-           #Incrémenter le nombre pour aller trouver le n palindrom suivant
-            n[]
-        else:
-            decompser(n)
-    else : 
-        print("not palindrome")
+
+largestPalindrome3Digit(899998)
+
+"""
+89 99 98
+89 88 98
+89 77 98
+89 66 98
+.. .. ..
+89 00 98
+87 99 78 
+.. .. ..
+87 00 78 
+86 00 68
+80 00 08
+"""
