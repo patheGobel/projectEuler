09d70b6a1ecee9875f365d08a9583c2cebeabfb3
Pathe <pathegba@gmail.com>
2023-11-03 00:42:33 +0000

    Construction de la Fonction largestPalindrome3Digit(n). Mais je me demande si je vais pas crée d'abord une fonction isPalindrom() d'abord ?

---
+++ tester.py
@@ -0,0 +1,12 @@
+from math import sqrt
+def largestPalindrome3Digit(n):
+    n = str(n)
+    if n[0:2] == n[:-2]:
+        print("palindrome")
+        if isPremier(n):
+           #Incrémenter le nombre pour aller trouver le n palindrom suivant
+            n[]
+        else:
+            decompser(n)
+    else : 
+        print("not palindrome")
